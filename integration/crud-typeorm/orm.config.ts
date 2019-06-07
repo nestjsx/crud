ac0603685b3370d008ecac8f69468dc94d1740d3
Michael Yali <mihon4ik@gmail.com>
2019-06-07 21:26:16 +0300

    ci: docker-compose

--- integration/crud-typeorm/orm.config.ts
+++ integration/crud-typeorm/orm.config.ts
@@ -4,7 +4,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const withCache: TypeOrmModuleOptions = {
   type: 'postgres',
   host: '127.0.0.1',
-  port: 5432,
+  port: 5455,
   username: 'root',
   password: 'root',
   database: 'nestjsx_crud',
@@ -14,7 +14,7 @@ export const withCache: TypeOrmModuleOptions = {
     type: 'redis',
     options: {
       host: '127.0.0.1',
-      port: 6379,
+      port: 6399,
     },
   },
   entities: [join(__dirname, './**/*.entity{.ts,.js}')],
