e9bc747928ca42062802bd9c05f8283ef4a45f81
hisham <hishamshikha96@gmail.com>
2019-09-25 13:52:57 +0300

    fix(service): add sequelize db to docker compose

--- integration/crud-sequelize/orm.config.ts
+++ integration/crud-sequelize/orm.config.ts
@@ -3,7 +3,7 @@ import { SequelizeOptions } from 'sequelize-typescript';
 export const config: SequelizeOptions = {
   dialect: 'postgres',
   host: '127.0.0.1',
-  port: 5455,
+  port: 5456,
   username: 'root',
   password: 'root',
   database: 'nestjsx_crud_sequelize',
