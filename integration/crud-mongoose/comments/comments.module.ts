d920292a09aec5f56fcd9602c150e9434f04a9d8
Jay Bell <jaycooperbell@gmail.com>
2019-12-23 16:11:40 -0800

    fix(getmany): use lean to coerce mongoose documents to JSON
    - `getMany` will return an array of JSON objects now instead of array of Mongoose documents

--- integration/crud-mongoose/comments/comments.module.ts
+++ integration/crud-mongoose/comments/comments.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { CommentsController } from './comments.controller';
+import { CommentsService } from './comments.service';
 
 @Module({
   controllers: [
-    Comment
+    CommentsController
+  ],
+  providers: [
+    CommentsService
   ]
 })
+export class CommentsModule {}
