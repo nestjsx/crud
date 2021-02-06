278dcc91f5a9ef79f2c76ec30bb7e346e2796128
Michael Yali <mihon4ik@gmail.com>
2021-02-06 15:57:46 +0200

    fix(crud): updated CrudRoutesFactory methods visibility

--- packages/crud/src/decorators/crud.decorator.ts
+++ packages/crud/src/decorators/crud.decorator.ts
@@ -2,6 +2,7 @@ import { CrudRoutesFactory } from '../crud';
 import { CrudOptions } from '../interfaces';
 
 export const Crud = (options: CrudOptions) => (target: Object) => {
-  let factory = CrudRoutesFactory.create(target, options);
+  const factoryMethod = options.routesFactory || CrudRoutesFactory;
+  let factory = new factoryMethod(target, options);
   factory = undefined;
 };
