e09ecfd12a2eb7aa772f47b4d341c1ef02d02ee6
Hoa Vo Van <vvhoa@axonactive.com>
2022-03-01 16:38:02 +0700

    Add mariadb to getFieldWithAlias

--- packages/crud-typeorm/src/typeorm-crud.service.ts
+++ packages/crud-typeorm/src/typeorm-crud.service.ts
@@ -871,7 +871,7 @@ export class TypeOrmCrudService<T> extends CrudService<T> {
 
   protected getFieldWithAlias(field: string, sort: boolean = false) {
     /* istanbul ignore next */
-    const i = this.dbName === 'mysql' ? '`' : '"';
+    const i = ['mysql','mariadb'].includes(this.dbName) ? '`' : '"';
     const cols = field.split('.');
 
     switch (cols.length) {
